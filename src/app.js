f035266705b8271551de65c6f87a45a441ba363e
scarlett3933-sketch <scarlett3933@gmail.com>
2026-04-19 21:48:40 -0700

    初始化，有方块

---
+++ src/app.js
@@ -0,0 +1,257 @@
+import * as THREE from 'three';
+import { VRButton } from 'three/addons/webxr/VRButton.js';
+import { XRControllerModelFactory } from 'three/addons/webxr/XRControllerModelFactory.js';
+import Stats from 'three/addons/libs/stats.module.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+   document.addEventListener("DOMContentLoaded", function(){
+                const app = new App();
+                window.app = app;
+            });
+
+class App{
+	constructor(){
+		const container = document.createElement( 'div' );
+		document.body.appendChild( container );
+        
+        this.clock = new THREE.Clock();
+        
+		this.camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 200 );
+		this.camera.position.set( 0, 1.6, 5 );
+        
+		this.scene = new THREE.Scene();
+        this.scene.background = new THREE.Color( 0x505050 );
+
+		this.scene.add( new THREE.HemisphereLight( 0xffffff, 0x404040 ) );
+
+        const light = new THREE.DirectionalLight( 0xffffff, 6 );
+        light.position.set( 1, 1, 1 ).normalize();
+		this.scene.add( light );
+			
+		this.renderer = new THREE.WebGLRenderer({ antialias: true } );
+		this.renderer.setPixelRatio( window.devicePixelRatio );
+		this.renderer.setSize( window.innerWidth, window.innerHeight );
+        this.renderer.outputEncoding = THREE.sRGBEncoding;
+        
+		container.appendChild( this.renderer.domElement );
+        
+        this.controls = new OrbitControls( this.camera, this.renderer.domElement );
+        this.controls.target.set(0, 1.6, 0);
+        this.controls.update();
+        
+        this.stats = new Stats();
+
+        this.tmpQuat = new THREE.Quaternion();//旋转用，暂时储存
+        
+        this.raycaster = new THREE.Raycaster();//手柄射线
+        this.workingMatrix = new THREE.Matrix4();
+        this.workingVector = new THREE.Vector3();
+        this.origin = new THREE.Vector3();
+        
+        this.initScene();
+        this.setupVR();
+        
+        window.addEventListener('resize', this.resize.bind(this) );
+        
+        this.renderer.setAnimationLoop( this.render.bind(this) );
+	}	
+    
+    random( min, max ){
+        return Math.random() * (max-min) + min;
+    }
+    
+    initScene(){
+
+		this.scene.background = new THREE.Color( 0xa0a0a0 );
+		this.scene.fog = new THREE.Fog( 0xa0a0a0, 50, 100 );
+
+		// ground
+		const ground = new THREE.Mesh( new THREE.PlaneGeometry( 200, 200 ), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
+		ground.rotation.x = - Math.PI / 2;
+		this.scene.add( ground );
+
+		var grid = new THREE.GridHelper( 200, 40, 0x000000, 0x000000 );
+		grid.material.opacity = 0.2;
+		grid.material.transparent = true;
+		this.scene.add( grid );
+        
+        const geometry = new THREE.BoxGeometry(5, 5, 5);
+        const material = new THREE.MeshStandardMaterial({ color:0xAAAA22 });
+        const edges = new THREE.EdgesGeometry( geometry );
+        const line = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0x000000, linewidth: 2 } ) );
+        
+        //建了很多方块
+        this.colliders = [];
+        
+        for (let x=-100; x<100; x+=10){
+            for (let z=-100; z<100; z+=10){
+                if (x==0 && z==0) continue;
+                const box = new THREE.Mesh(geometry, material);
+                box.position.set(x, 2.5, z);
+                const edge = line.clone();
+                edge.position.copy( box.position );
+                this.scene.add(box);
+                this.scene.add(edge);
+                this.colliders.push(box);
+            }
+        }
+        
+    } 
+    
+    setupVR(){
+        this.renderer.xr.enabled = true;
+        
+        const button = new VRButton( this.renderer );
+        
+        const self = this;
+        
+        function onSelectStart() {
+            
+            this.userData.selectPressed = true;
+        }
+
+        function onSelectEnd() {
+
+            this.userData.selectPressed = false;
+            
+        }
+        
+        this.controller = this.renderer.xr.getController( 0 );
+        this.controller.addEventListener( 'selectstart', onSelectStart );
+        this.controller.addEventListener( 'selectend', onSelectEnd );
+        this.controller.addEventListener( 'connected', function ( event ) {
+
+            const mesh = self.buildController.call(self, event.data );
+            mesh.scale.z = 0;
+            this.add( mesh );
+
+        } );
+        this.controller.addEventListener( 'disconnected', function () {
+
+            this.remove( this.children[ 0 ] );
+            self.controller = null;
+            self.controllerGrip = null;
+
+        } );
+        this.scene.add( this.controller );
+
+        const controllerModelFactory = new XRControllerModelFactory();
+
+        this.controllerGrip = this.renderer.xr.getControllerGrip( 0 );
+        this.controllerGrip.add( controllerModelFactory.createControllerModel( this.controllerGrip ) );
+        this.scene.add( this.controllerGrip );//导入手柄样式
+        
+        //
+        //运动camera，放进dolly里
+        this.dolly = new THREE.Object3D(); 
+        this.dolly.position.z = 5;
+        this.dolly.add( this.camera );
+        this.scene.add( this.dolly );
+        
+        this.dummyCam = new THREE.Object3D();
+        this.camera.add( this.dummyCam );
+
+    }
+    
+    buildController( data ) {
+        let geometry, material;
+        
+        switch ( data.targetRayMode ) {
+            
+            case 'tracked-pointer':
+
+                geometry = new THREE.BufferGeometry();
+                geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( [ 0, 0, 0, 0, 0, - 1 ], 3 ) );
+                geometry.setAttribute( 'color', new THREE.Float32BufferAttribute( [ 0.5, 0.5, 0.5, 0, 0, 0 ], 3 ) );
+
+                material = new THREE.LineBasicMaterial( { vertexColors: true, blending: THREE.AdditiveBlending } );
+
+                return new THREE.Line( geometry, material );
+
+            case 'gaze':
+
+                geometry = new THREE.RingGeometry( 0.02, 0.04, 32 ).translate( 0, 0, - 1 );
+                material = new THREE.MeshBasicMaterial( { opacity: 0.5, transparent: true } );
+                return new THREE.Mesh( geometry, material );
+
+        }
+
+    }
+    
+    handleController( controller, dt ){
+        if (controller.userData.selectPressed ){
+            
+            const wallLimit = 1.3;
+            const speed = 2;
+            let pos = this.dolly.position.clone();
+            pos.y += 1;//让他自由动起来
+
+            let dir = new THREE.Vector3();
+
+
+            //Store original dolly rotation，头部旋转方向
+            const quaternion = this.dolly.quaternion.clone();//orriantation 
+            
+            //Get rotation for movement from the headset pose
+            this.dolly.quaternion.copy( this.dummyCam.getWorldQuaternion( this.tmpQuat ) );//目前头的转向复制给camera dolly的转向
+            this.dolly.getWorldDirection(dir);//获取dolly的转向
+            dir.negate();//把方向反过来
+            this.raycaster.set(pos, dir);
+
+            let blocked = false;
+
+            let intersect = this.raycaster.intersectObjects(this.colliders);
+            if (intersect.length>0){
+                if (intersect[0].distance < wallLimit) blocked = true;
+            }
+
+            if (!blocked){
+                this.dolly.translateZ(-dt*speed);
+                pos = this.dolly.getWorldPosition( this.origin );
+            }//如果遇到东西会挡住
+
+            //cast left，no physics engine版，左右检测防撞墙
+            dir.set(-1,0,0);
+            dir.applyMatrix4(this.dolly.matrix);
+            dir.normalize();
+            this.raycaster.set(pos, dir);
+
+            intersect = this.raycaster.intersectObjects(this.colliders);
+            if (intersect.length>0){
+                if (intersect[0].distance<wallLimit) this.dolly.translateX(wallLimit-intersect[0].distance);
+            }
+
+            //cast right
+            dir.set(1,0,0);
+            dir.applyMatrix4(this.dolly.matrix);
+            dir.normalize();
+            this.raycaster.set(pos, dir);
+
+            intersect = this.raycaster.intersectObjects(this.colliders);
+            if (intersect.length>0){
+                if (intersect[0].distance<wallLimit) this.dolly.translateX(intersect[0].distance-wallLimit);
+            }
+
+            this.dolly.position.y = 0;
+
+            //Restore the original rotation
+            this.dolly.quaternion.copy( quaternion );
+   
+        }
+    }
+    
+    resize(){
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize( window.innerWidth, window.innerHeight );  
+    }
+    
+	render( ) {  
+        const dt = this.clock.getDelta();//时间差
+        this.stats.update();
+        if (this.controller ) this.handleController( this.controller, dt );
+        this.renderer.render( this.scene, this.camera );
+    }
+}
+
+export { App };
